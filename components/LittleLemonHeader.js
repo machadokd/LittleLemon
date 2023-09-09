17898ea7cf5ae5a82d0fabc6fca38a0ae1811e02
joão machado <machado@MBP-de-joao.lan>
2023-09-09 15:42:31 +0100

    add last changes

--- components/LittleLemonHeader.js
+++ components/LittleLemonHeader.js
@@ -1,17 +1,22 @@
-import { View, Text } from 'react-native';
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function LittleLemonHeader() {
     return (
-        <View style={{ flex: 0.15, backgroundColor: '#F4CE14' }}>
-            <Text
-                style={{
-                    padding: 40,
-                    fontSize: 30,
-                    color: 'black',
-                    textAlign: 'center',
-                }}>
-                Little Lemon
-            </Text>
+        <View style={styles.container}>
+            <Text style={styles.headerText}>Little Lemon</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#EE9972',
+    },
+    headerText: {
+        padding: 40,
+        fontSize: 30,
+        color: 'black',
+        textAlign: 'center',
+    },
+});
